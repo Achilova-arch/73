d70b1b6d725af874e87b10fb53b2fe90e79e4012
Achilova-arch <asalachilova96@gmail.com>
2025-09-13 08:59:55 +0500

    Add files via upload

---
+++ 4/main.py
@@ -0,0 +1,13 @@
+import math
+
+print(math.pow(3,4))
+
+print(math.sqrt(16))
+
+print(math.floor(1/6)) 
+print(math.ceil(7/5)) 
+print(round(8/5)) 
+
+print(math.factorial(5))
+
+print(math.fabs(5)) 
